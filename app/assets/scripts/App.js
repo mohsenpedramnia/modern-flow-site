cf144b51f680091840dc66e352ea8526ccd86dc4
mohsenpedramnia <mohsen.pedramnia@gmail.com>
2020-06-09 11:08:23 +0300

    Completed our build process configuration.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -6,6 +6,7 @@ import StickyHeader from './modules/StickyHeader';
 
 
 
+
 new StickyHeader();
  new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
  new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
